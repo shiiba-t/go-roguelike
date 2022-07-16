6b6091c11811940cdacc973312da43c83d16b7fc
shiiba-t <t.shiiba1210@gmail.com>
2022-07-16 18:52:01 +0900

    Roguelike Tutorial Part1 終了

--- level.go
+++ level.go
@@ -1,1 +1,78 @@
 package main
+
+import (
+	"log"
+
+	_ "image/png" // 裏で勝手にデコードしてくれるようにする
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type GameData struct {
+	ScreenWidth  int
+	ScreenHeight int
+	TileWidth    int
+	TileHeight   int
+}
+
+func NewGameData() GameData {
+	g := GameData{
+		ScreenWidth:  80,
+		ScreenHeight: 50,
+		TileWidth:    16,
+		TileHeight:   16,
+	}
+	return g
+}
+
+type MapTile struct {
+	PixelX  int
+	PixelY  int
+	Blocked bool
+	Image   *ebiten.Image
+}
+
+//GetIndexFromXY gets the index of the map array from a given X,Y TILE coordinate.
+//This coordinate is logical tiles, not pixels.
+func GetIndexFromXY(x, y int) int {
+	gd := NewGameData()
+	return (y * gd.ScreenWidth) + x
+}
+
+func CreateTiles() []MapTile {
+	gd := NewGameData()
+	tiles := make([]MapTile, 0)
+
+	for x := 0; x < gd.ScreenWidth; x++ {
+		for y := 0; y < gd.ScreenHeight; y++ {
+			if x == 0 || x == gd.ScreenWidth-1 || y == 0 || y == gd.ScreenHeight-1 {
+				wall, _, err := ebitenutil.NewImageFromFile("assets/wall.png")
+				if err != nil {
+					log.Fatal(err)
+				}
+				tile := MapTile{
+					PixelX:  x * gd.TileWidth,
+					PixelY:  y * gd.TileHeight,
+					Blocked: true,
+					Image:   wall,
+				}
+				tiles = append(tiles, tile)
+			} else {
+				floor, _, err := ebitenutil.NewImageFromFile("assets/floor.png")
+				if err != nil {
+					log.Fatal(err)
+				}
+				tile := MapTile{
+					PixelX:  x * gd.TileWidth,
+					PixelY:  y * gd.TileHeight,
+					Blocked: false,
+					Image:   floor,
+				}
+				tiles = append(tiles, tile)
+			}
+		}
+	}
+
+	return tiles
+}
--- main.go
+++ main.go
@@ -8,12 +8,14 @@ import (
 
 //Game holds all data the entire game will need.
 type Game struct {
+	Tiles []MapTile
 }
 
 //NewGame creates a new Game Object and initializes the data
 //This is a pretty solid refactor candidate for later
 func NewGame() *Game {
 	g := &Game{}
+	g.Tiles = CreateTiles()
 	return g
 }
 
@@ -24,6 +26,17 @@ func (g *Game) Update() error {
 
 //Draw is called each draw cycle and is where we will blit.
 func (g *Game) Draw(screen *ebiten.Image) {
+	gd := NewGameData()
+
+	// Draw the Map
+	for x := 0; x < gd.ScreenWidth; x++ {
+		for y := 0; y < gd.ScreenHeight; y++ {
+			tile := g.Tiles[GetIndexFromXY(x, y)]
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(float64(tile.PixelX), float64(tile.PixelY))
+			screen.DrawImage(tile.Image, op)
+		}
+	}
 }
 
 //Layout will return the screen dimensions.
