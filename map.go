c0c21dbd4e3d311dfb4e3e16761cdff00b27de5b
shiiba-t <t.shiiba1210@gmail.com>
2022-07-17 11:57:49 +0900

    Roguelike Tutorial Part4 終了

--- map.go
+++ map.go
@@ -2,7 +2,8 @@ package main
 
 //GameMap holds all the level and aggregate information for the entire world.
 type GameMap struct {
-	Dungeons []Dungeon
+	Dungeons     []Dungeon
+	CurrentLevel Level
 }
 
 func NewGameMap() GameMap {
@@ -12,6 +13,6 @@ func NewGameMap() GameMap {
 	d := Dungeon{Name: "default", Levels: levels}
 	dungeons := make([]Dungeon, 0)
 	dungeons = append(dungeons, d)
-	gm := GameMap{Dungeons: dungeons}
+	gm := GameMap{Dungeons: dungeons, CurrentLevel: l}
 	return gm
 }
