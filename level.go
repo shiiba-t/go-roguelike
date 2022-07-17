99abef3bd758d1e89dfdb223b70d2fa6c4a66f7d
shiiba-t <t.shiiba1210@gmail.com>
2022-07-17 12:33:54 +0900

    Roguelike Tutorial Part5 終了

--- level.go
+++ level.go
@@ -19,12 +19,16 @@ type MapTile struct {
 //Level holds the tile information for a complete dungeon level.
 type Level struct {
 	Tiles []MapTile
+	Rooms []Rect
 }
 
 func NewLevel() Level {
 	l := Level{}
-	tiles := l.CreateTiles()
+	tiles := l.createTiles()
 	l.Tiles = tiles
+	rooms := make([]Rect, 0)
+	l.Rooms = rooms
+	l.GenerateLevelTiles()
 	return l
 }
 
@@ -35,7 +39,7 @@ func (level *Level) GetIndexFromXY(x, y int) int {
 	return (y * gd.ScreenWidth) + x
 }
 
-func (level *Level) CreateTiles() []MapTile {
+func (level *Level) createTiles() []MapTile {
 	gd := NewGameData()
 	tiles := make([]MapTile, gd.ScreenHeight*gd.ScreenWidth)
 	index := 0
@@ -43,35 +47,66 @@ func (level *Level) CreateTiles() []MapTile {
 	for x := 0; x < gd.ScreenWidth; x++ {
 		for y := 0; y < gd.ScreenHeight; y++ {
 			index = level.GetIndexFromXY(x, y)
-			if x == 0 || x == gd.ScreenWidth-1 || y == 0 || y == gd.ScreenHeight-1 {
-				wall, _, err := ebitenutil.NewImageFromFile("assets/wall.png")
-				if err != nil {
-					log.Fatal(err)
-				}
-				tile := MapTile{
-					PixelX:  x * gd.TileWidth,
-					PixelY:  y * gd.TileHeight,
-					Blocked: true,
-					Image:   wall,
-				}
-				tiles[index] = tile
-			} else {
-				floor, _, err := ebitenutil.NewImageFromFile("assets/floor.png")
-				if err != nil {
-					log.Fatal(err)
-				}
-				tile := MapTile{
-					PixelX:  x * gd.TileWidth,
-					PixelY:  y * gd.TileHeight,
-					Blocked: false,
-					Image:   floor,
-				}
-				tiles[index] = tile
+			wall, _, err := ebitenutil.NewImageFromFile("assets/wall.png")
+			if err != nil {
+				log.Fatal(err)
 			}
+			tile := MapTile{
+				PixelX:  x * gd.TileWidth,
+				PixelY:  y * gd.TileHeight,
+				Blocked: true,
+				Image:   wall,
+			}
+			tiles[index] = tile
 		}
 	}
 
 	return tiles
+}
+
+func (level *Level) createRoom(room Rect) {
+	for y := room.Y1 + 1; y < room.Y2; y++ {
+		for x := room.X1 + 1; x < room.X2; x++ {
+			index := level.GetIndexFromXY(x, y)
+			level.Tiles[index].Blocked = false
+			floor, _, err := ebitenutil.NewImageFromFile("assets/floor.png")
+			if err != nil {
+				log.Fatal(err)
+			}
+			level.Tiles[index].Image = floor
+		}
+	}
+}
+
+func (level *Level) GenerateLevelTiles() {
+	MIN_SIZE := 6
+	MAX_SIZE := 10
+	MAX_ROOMS := 30
+
+	gd := NewGameData()
+	tiles := level.createTiles()
+	level.Tiles = tiles
+
+	for idx := 0; idx < MAX_ROOMS; idx++ {
+		w := GetRandamBetween(MIN_SIZE, MAX_SIZE)
+		h := GetRandamBetween(MIN_SIZE, MAX_SIZE)
+		x := GetDiceRoll(gd.ScreenWidth-w-1) - 1
+		y := GetDiceRoll(gd.ScreenHeight-h-1) - 1
+
+		new_room := NewRect(x, y, w, h)
+		okToAdd := true
+
+		for _, otherRoom := range level.Rooms {
+			if new_room.Intersect(otherRoom) {
+				okToAdd = false
+				break
+			}
+		}
+		if okToAdd {
+			level.createRoom(new_room)
+			level.Rooms = append(level.Rooms, new_room)
+		}
+	}
 }
 
 func (level *Level) DrawLevel(screen *ebiten.Image) {
